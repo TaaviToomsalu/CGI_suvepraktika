40541d161ebbc1a65e4633557c2f72e27cff9b20
Taavi Toomsalu <queenshelob@mail.com>
2024-03-20 21:56:18 +0200

    There are now 7 days filled with movies, also user viewing history takes movieTitle instead of userId

--- src/main/java/movieapp/model/Movie.java
+++ src/main/java/movieapp/model/Movie.java
@@ -13,34 +13,86 @@ public class Movie {
     private int id;
     private String title;
     private String genre;
-    private int duration;
     private String ageRating;
     private LocalTime startTime;
     private String language;
+    private DayOfWeek dayOfWeek;
 
     //Default constructor
     public Movie() {
     }
 
     //Constructor
-    public Movie(int id, String title, String genre, int duration, String ageRating, LocalTime startTime, String language) {
+    public Movie(int id, String title, String genre, String ageRating, LocalTime startTime, String language, DayOfWeek dayOfWeek) {
         this.id = id;
         this.title = title;
         this.genre = genre;
-        this.duration = duration;
         this.ageRating = ageRating;
         this.startTime = startTime;
         this.language = language;
+        this.dayOfWeek = dayOfWeek;
     }
 
     //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 
     public String getGenre() {
         return genre;
     }
 
+    public String getAgeRating() {
+        return ageRating;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    // Getter and setter for dayOfWeek
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
 
     // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setAgeRating(String ageRating) {
+        this.ageRating = ageRating;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
     // Override toString method
     @Override
     public String toString() {
@@ -48,18 +100,9 @@ public class Movie {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", genre='" + genre + '\'' +
-                ", duration='" + duration + '\'' +
+                ", ageRating='" + ageRating + '\'' +
+                ", startTime=" + startTime +
+                ", language='" + language + '\'' +
                 '}';
     }
-
-    public String getAgeRating() {
-        return ageRating;
-    }
-
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
 }
-
